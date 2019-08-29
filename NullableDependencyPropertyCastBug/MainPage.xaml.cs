11e6b4280c2c35da0675873ddfa5e7bd8507759e
Anthony Russell <alrussellmain@gmail.com>
2019-08-28 22:21:01 -0400

    initial commit of bug code

---
+++ NullableDependencyPropertyCastBug/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Windows.UI.Xaml.Controls;
+namespace NullableDependencyPropertyCastBug
+{
+    public sealed partial class MainPage : Page
+    {
+        public decimal? ViewModelTestDecimal { get; set; }
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.DataContext = this;
+        }
+    }
+}
---
+++ NullableDependencyPropertyCastBug/TestControl.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+namespace NullableDependencyPropertyCastBug
+{
+    public sealed partial class TestControl : UserControl
+    {
+        public int? TestIntConversion
+        {
+            get { return TestDecimal.HasValue ? Decimal.ToInt32(TestDecimal.Value) : 0; }
+            set { TestDecimal = (decimal)value; }
+        }        
+
+        public decimal? TestDecimal
+        {
+            get { return (decimal?)GetValue(TestDecimalProperty); }
+            set { SetValue(TestDecimalProperty, value); }
+        }
+
+        public static readonly DependencyProperty TestDecimalProperty =
+            DependencyProperty.Register("TestDecimal", typeof(object), typeof(TestControl), new PropertyMetadata(0));
+
+        
+        public TestControl()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
